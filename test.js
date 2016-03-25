f42797a20137153452af3a045994293bb8f0ee64
thisman <tablescable@gmail.com>
2016-03-25 06:41:05 +0000

    add more mocha test

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var should = require('should');
 var URLQueryBuilder = require('./index.js');
 
 var siteUrl = "example.com";
@@ -15,6 +16,14 @@ describe('Url Query Builder', function() {
 			url = qBuilder.add("name2", "value2").getUrl();
 			assert.equal("example.com?name=value&name2=value2&", url);
 		});
+
+		it('should throw error trying add query that already exist', function () {
+			(function () {
+				var qBuilder = new URLQueryBuilder(siteUrl);
+				var url = qBuilder.add("name", "value")
+					.add("name", "value")
+			}).should.throw();		
+		})
 	});
 
 
@@ -27,6 +36,14 @@ describe('Url Query Builder', function() {
 
 			assert.equal("example.com?name=newValue&", url);
 		});
+
+		it('should throw error trying change query that not exist', function () {
+			(function () {
+				var qBuilder = new URLQueryBuilder(siteUrl);
+				var url = qBuilder.add("name", "value")
+					.change("name2", "value")
+			}).should.throw();		
+		})
 	});
 
 	describe('#delete()', function () {
@@ -38,6 +55,14 @@ describe('Url Query Builder', function() {
 
 			assert.equal("example.com?", url);
 		});
+
+		it('should throw error trying delete query that not exist', function () {
+			(function () {
+				var qBuilder = new URLQueryBuilder(siteUrl);
+				var url = qBuilder.add("name", "value")
+					.delete("name2", "value")
+			}).should.throw();		
+		})
 	});
 
 });

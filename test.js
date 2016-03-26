cdf8623cab5f6543d8a2f1b8edb36bc7ba07970c
thisman <tablescable@gmail.com>
2016-03-26 13:24:59 +0000

    update tes

--- test.js
+++ test.js
@@ -10,23 +10,23 @@ describe('Url Query Builder', function() {
 	describe('#init', function () {
 		it('should parse initial query from url', function () {
 			var qBuilder = new URLQueryBuilder(siteUrl + "?name=value");
-			var url = qBuilder.add("name2", "value2").add({cost: 100}).getUrl();
 
-			assert.equal("example.com?name=value&name2=value2&cost=100&", url);
+			assert.equal("example.com?name=value&", qBuilder.getUrl() );
 		});
 
 		it('should parse inital query by @param {string} query', function () {
 			var qBuilder = new URLQueryBuilder(siteUrl, "name=value");
-			var url = qBuilder.getUrl();
 
-			assert.equal("example.com?name=value&", url);
+			assert.equal("example.com?name=value&", qBuilder.getUrl() );
 		});
 
 		it('should parse inital query by @param {object} query', function () {
-			var qBuilder = new URLQueryBuilder(siteUrl, {"name": "value", "name2":"value2"});
-			var url = qBuilder.getUrl();
+			var qBuilder = new URLQueryBuilder(siteUrl, {
+				"name": "value", 
+				"name2":"value2"
+			});
 
-			assert.equal("example.com?name=value&name2=value2&", url);
+			assert.equal("example.com?name=value&name2=value2&", qBuilder.getUrl() );
 		});
 
 		it('should not throw an exception when initialization without params', function () {
@@ -35,16 +35,15 @@ describe('Url Query Builder', function() {
 			}).should.not.throw();
 		});
 
-		it('don"t added empty query after last &', function () {
+		it("don't added empty query after last &", function () {
 			var qBuilder = new URLQueryBuilder(siteUrl + "?name=value&");
-			var url = qBuilder.getUrl();
 
-			assert.equal("example.com?name=value&", url);
+			assert.equal("example.com?name=value&", qBuilder.getUrl() );
 		})
 	});
 
 	describe('#add()', function () {
-		it('should add query string to url', function () {
+		it('should add query string', function () {
 			var qBuilder = new URLQueryBuilder(siteUrl);
 			var url = qBuilder.add("name", "value").getUrl();
 			assert.equal( "example.com?name=value&", url);
@@ -53,15 +52,7 @@ describe('Url Query Builder', function() {
 			assert.equal("example.com?name=value&name2=value2&", url);
 		});
 
-		it('should throw error trying add query that already exist', function () {
-			(function () {
-				var qBuilder = new URLQueryBuilder(siteUrl);
-				var url = qBuilder.add("name", "value")
-					.add("name", "value")
-			}).should.throw();		
-		})
-
-		it('should add query object to url', function () {
+		it('should add query object', function () {
 			var qBuilder = new URLQueryBuilder(siteUrl);
 			var url = qBuilder.add({"name": "value"}).getUrl();
 			assert.equal( "example.com?name=value&", url);
@@ -69,18 +60,10 @@ describe('Url Query Builder', function() {
 			url = qBuilder.add({"name2" : "value2"}).getUrl();
 			assert.equal("example.com?name=value&name2=value2&", url);
 		});
-
-		it('should throw error trying add query {object} that already exist', function () {
-			(function () {
-				var qBuilder = new URLQueryBuilder(siteUrl);
-				var url = qBuilder.add("name", "value")
-					.add({"name" : "value"})
-			}).should.throw();		
-		})
 	});
 
 	describe('#change()', function () {
-		it('should change query string to url', function () {
+		it('should change query string', function () {
 			var qBuilder = new URLQueryBuilder(siteUrl);
 			var url = qBuilder.add("name", "value")
 				.change("name", "newValue")
@@ -88,18 +71,10 @@ describe('Url Query Builder', function() {
 
 			assert.equal("example.com?name=newValue&", url);
 		});
-
-		it('should throw error trying change query that not exist', function () {
-			(function () {
-				var qBuilder = new URLQueryBuilder(siteUrl);
-				var url = qBuilder.add("name", "value")
-					.change("name2", "value")
-			}).should.throw();		
-		})
 	});
 
 	describe('#delete()', function () {
-		it('should delete query string to url', function () {
+		it('should delete query string', function () {
 			var qBuilder = new URLQueryBuilder(siteUrl);
 			var url = qBuilder.add("name", "value")
 				.delete("name")
@@ -107,14 +82,6 @@ describe('Url Query Builder', function() {
 
 			assert.equal("example.com?", url);
 		});
-
-		it('should throw error trying delete query that not exist', function () {
-			(function () {
-				var qBuilder = new URLQueryBuilder(siteUrl);
-				var url = qBuilder.add("name", "value")
-					.delete("name2", "value")
-			}).should.throw();		
-		})
 	});
 
 	describe('#reset()', function () {
